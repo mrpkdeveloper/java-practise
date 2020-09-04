f7fb2670eec3fc6b0233fa84c243c13c082e8d22
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-09-04 23:44:36 +0530

    hollow diamond pattern

---
+++ src/patternchallenges/hollowdiamond.java
@@ -0,0 +1,18 @@
+package patternchallenges;
+
+import java.util.Scanner;
+
+public class hollowdiamond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int r = 0, nsp=0;
+		while (r < n) {
+			
+
+		}
+	}
+
+}
