8a9347eae1c6ee7c98ba27c3a9525bc6a179915a
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-09-14 00:00:03 +0530

    bubble sort

---
+++ src/array/bubblesort.java
@@ -0,0 +1,31 @@
+package array;
+
+public class bubblesort {
+
+	public static void bubblesort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					arr[j] = arr[j + 1] + arr[j];
+					arr[j + 1] = arr[j] - arr[j + 1];
+					arr[j] = arr[j] - arr[j + 1];
+				}
+			}
+		}
+	}
+
+	public static void disp(int[] arr) {
+		for (int val : arr) {
+			System.out.println(val);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		bubblesort(arr);
+		disp(arr);
+
+	}
+
+}
