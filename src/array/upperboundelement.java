e446ed5e7443f3f750b0dde956b3729f6be27964
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-09-16 00:16:18 +0530

    upper bound elemment

---
+++ src/array/upperboundelement.java
@@ -0,0 +1,29 @@
+package array;
+
+public class upperboundelement {
+
+	public static int lowerbound(int[] arr, int n) {
+		int ans = -1;
+		int l = 0, r = arr.length - 1, mid = (l + r) / 2;
+		while (l <= r) {
+			if (n == arr[mid]) {
+				l = mid + 1;
+				ans = mid;
+			} else if (n > arr[mid]) {
+				l = mid + 1;
+			} else {
+				r = mid - 1;
+			}
+			mid = (l + r) / 2;
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 5, 5, 5, 5, 5, 5, 6 };
+		System.out.println(lowerbound(arr, 5));
+
+	}
+
+}
