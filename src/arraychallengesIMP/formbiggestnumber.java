8479d925f317c6f1dacacd216ac7984346f45551
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-10-12 23:52:24 +0530

    biggest number

---
+++ src/arraychallengesIMP/formbiggestnumber.java
@@ -0,0 +1,15 @@
+package arraychallengesIMP;
+
+public class formbiggestnumber {
+
+	public static void biggestnumber(int arr) {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 54, 546, 548, 60 };
+
+	}
+
+}
