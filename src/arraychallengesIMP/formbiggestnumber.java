604864c53b3895e9f37a8f0cc70fcb0b77d9aacd
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-10-13 21:32:23 +0530

    largest number

--- src/arraychallengesIMP/formbiggestnumber.java
+++ src/arraychallengesIMP/formbiggestnumber.java
@@ -1,14 +1,41 @@
 package arraychallengesIMP;
 
+import java.util.Scanner;
+
 public class formbiggestnumber {
 
-	public static void biggestnumber(int arr) {
-
+	public static void biggestnumber(int[] arr) {
+		String finalno = "";
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				String ij = "" + arr[i] + arr[j];
+				String ji = "" + arr[j] + arr[i];
+				if (ij.compareTo(ji) < 0) {
+					int sum = arr[i] + arr[j];
+					arr[i] = sum - arr[i];
+					arr[j] = sum - arr[i];
+				}
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			finalno += arr[i];
+		}
+		System.out.println(finalno);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = { 54, 546, 548, 60 };
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		while (t > 0) {
+			int m = scn.nextInt();
+			int[] arr = new int[m];
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = scn.nextInt();
+			}
+			biggestnumber(arr);
+			t--;
+		}
 
 	}
 
