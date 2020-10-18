fe0dc1123b0d141498a0caa54edb5406c7ef0ea2
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-10-19 00:47:27 +0530

    factorial of a large number

--- src/arraychallengesIMP/largefactorial.java
+++ src/arraychallengesIMP/largefactorial.java
@@ -1,19 +1,36 @@
 package arraychallengesIMP;
 
+import java.util.Scanner;
+
 public class largefactorial {
 
 	public static void largefact(int n) {
-		long[] arr = new long[n + 1];
+		int[] arr = new int[100000];
 		arr[0] = 1;
-		for (int i = 1; i < arr.length; i++) {
-			arr[i] = i * arr[i - 1];
+		int nod = 1;
+		int carry = 0;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < nod; j++) {
+				int a = i * arr[j] + carry;
+				arr[j] = a % 10;
+				carry = a / 10;
+			}
+			while (carry > 0) {
+				arr[nod] = carry % 10;
+				carry = carry / 10;
+				nod++;
+			}
+
 		}
-		System.out.println(arr[n]);
+		for (int i = nod - 1; i >= 0; i--) {
+			System.out.print(arr[i]);
+		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 25;
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
 		largefact(n);
 
 	}
