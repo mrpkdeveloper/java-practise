d03e816106546efaf8b71c9a51cd06cdf462c089
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-11-03 19:29:46 +0530

    sanket and strings

--- src/stringchallenges/sanketstrings.java
+++ src/stringchallenges/sanketstrings.java
@@ -1,30 +1,45 @@
 package stringchallenges;
 
+import java.util.Scanner;
+
 import arraychallengesIMP.max;
 
 public class sanketstrings {
 
-	public static void maxsublength(String s, int n, char ch) {
+	public static int maxsublength(String s, int n, char ch) {
 		int l = 0, r = 0, c = 0, ans = 0;
-		while (r < s.length()) {
-			if (s.charAt(r) != ch && c < n) {
+		for (r = 0; r < s.length(); r++) {
+			if (s.charAt(r) != ch) {
 				c++;
 			}
-
 			if (c == n) {
 				break;
 			}
-			r++;
 		}
-		ans = r - l;
-		System.out.println(ans);
+		while (l < r) {
+			while (r < s.length() - 1 && s.charAt(r + 1) == ch) {
+				r++;
+			}
+			int curlength = r - l + 1;
+			ans = Math.max(ans, curlength);
+			l++;
+			if (s.charAt(l - 1) != ch && r < s.length() - 1) {
+				r++;
+			}
+		}
+
+		return ans;
 
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "aabbaab";
-		maxsublength(s, 2, 'a');
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		String s = scn.next();
+		int ansa = maxsublength(s, n, 'a');
+		int ansb = maxsublength(s, n, 'b');
+		System.out.println(Math.max(ansa, ansb));
 
 	}
 
