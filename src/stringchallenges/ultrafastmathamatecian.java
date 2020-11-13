46bee33af48ca31aaa3079b4f981d70c72324d71
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-11-13 23:35:07 +0530

    ultra fast maths

--- src/stringchallenges/ultrafastmathamatecian.java
+++ src/stringchallenges/ultrafastmathamatecian.java
@@ -1,18 +1,30 @@
 package stringchallenges;
 
+import java.util.Scanner;
+
 public class ultrafastmathamatecian {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 10111;
-		int m = 10000;
-		int ans = 0;
-		while (n != 0) {
-			int rem1 = n % 10;
-			int rem2 = m % 10;
-			if (rem1 == rem2) {
-				ans = ans + rem1;
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		while (t > 0) {
+			int n = scn.nextInt();
+			int m = scn.nextInt();
+			String ans = "";
+			while (n != 0) {
+				int rem1 = n % 10;
+				int rem2 = m % 10;
+				n = n / 10;
+				m = m / 10;
+				if (rem1 == rem2) {
+					ans = "0" + ans;
+				} else {
+					ans = "1" + ans;
+				}
 			}
+			System.out.println(ans);
+			t--;
 		}
 
 	}
